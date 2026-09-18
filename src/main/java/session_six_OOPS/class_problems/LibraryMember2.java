4be531b87971288c804e5f412f9ce8fc38131eaa
KaviPriya0707 <kavipriyam0707@gmail.com>
2026-09-19 00:31:56 +0530

    Create LibraryMember2.java

---
+++ src/main/java/session_six_OOPS/class_problems/LibraryMember2.java
@@ -0,0 +1,78 @@
+class LibraryMember {
+    protected String memberId;
+    protected int borrowLimit;
+    protected int booksBorrowed;
+
+    private int[] fineHistory = new int[10];
+    private int fineCount = 0;
+
+    public LibraryMember(String memberId, int borrowLimit) {
+        if (memberId == null || memberId.trim().length() < 4) {
+            throw new IllegalArgumentException("Invalid member ID");
+        }
+
+        this.memberId = memberId;
+        this.borrowLimit = borrowLimit;
+    }
+
+    protected void chargeFine(int amount) {
+        if (fineCount < fineHistory.length) {
+            fineHistory[fineCount] = amount;
+            fineCount++;
+        }
+    }
+
+    public int[] getFineHistory() {
+        int[] copy = new int[fineCount];
+
+        for (int i = 0; i < fineCount; i++) {
+            copy[i] = fineHistory[i];
+        }
+
+        return copy;
+    }
+
+    public int getTotalFine() {
+        int total = 0;
+
+        for (int i = 0; i < fineCount; i++) {
+            total += fineHistory[i];
+        }
+
+        return total;
+    }
+}
+
+class StudentMember extends LibraryMember {
+    private String course;
+
+    public StudentMember(String memberId, int borrowLimit,
+                          String course) {
+        super(memberId, borrowLimit);
+        this.course = course;
+    }
+
+    @Override
+    protected void chargeFine(int amount) {
+        super.chargeFine(amount / 2);
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        StudentMember s =
+            new StudentMember("STU5", 3, "CSE");
+
+        s.chargeFine(100);
+
+        System.out.println(s.getTotalFine());
+
+        int[] history = s.getFineHistory();
+
+        history[0] = 999;
+
+        int[] actualHistory = s.getFineHistory();
+
+        System.out.println(actualHistory[0]);
+    }
+}
